8be33d9d5b9de14a2736614ec25c43cc177cd724
MACKOzKLANU <sek-maciek@o2.pl>
2026-01-05 11:04:15 +0100

    add correct bootstrap file implementation

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,9 @@ import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import { BudgetProvider } from './contexts/BudgetContext.jsx'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// Custom css styles
-
-import './styles/bootstrap.js'
-
-import './styles/bootstrap.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
